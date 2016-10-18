44e86cb81382d51010e78fa0ec69529fd869193f
Agustin Becerra <beche29@gmail.com>
2016-10-18 15:47:09 -0300

    Primer commit

---
+++ app/src/main/java/com/example/agustin/mapasyuber/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.agustin.mapasyuber;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+//AIzaSyBEolRzoVUQ-w3_meukQgX6-3eGg38CBHI
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
